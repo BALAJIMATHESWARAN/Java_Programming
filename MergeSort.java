9342692705a18106bfdd97fc35dcc16008b36bd6
BALAJIMATHESWARAN <balajimatheswaran2004@gmail.com>
2025-07-13 14:52:17 +0530

    Updated code

--- MergeSort.java
+++ MergeSort.java
@@ -1,47 +1,51 @@
-public class MergeSort {
-    public static void mergeSort(int[] arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
-            merge(arr, left, mid, right);
+public class MergeSort{
+    public static void main(String[] args) {
+        int arr[] = {5,6,2,3,4,8,9,0};
+        mergeSort(arr,0,arr.length-1);
+        for(int i = 0 ; i < arr.length;i++){
+            System.out.print(arr[i] + " " );
         }
     }
 
-    private static void merge(int[] arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i = 0; i < n1; ++i)
-            L[i] = arr[left + i];
-        for (int j = 0; j < n2; ++j)
-            R[j] = arr[mid + 1 + j];
-
-        int i = 0, j = 0, k = left;
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr[k] = L[i];
+    public static void mergeSort(int arr[] , int l , int r){
+        if( l < r){
+            int mid = l + (r-l)/2;
+            mergeSort(arr, l ,mid);
+            mergeSort(arr, mid+1, r);
+            merger(arr, l , mid, r);
+        }
+    }
+    public static void merger(int arr[] , int l , int mid, int r){
+        int s = l;
+        int e = mid+1;
+        int res[] = new int[r -l +1];
+        int i = 0;
+        while(s <= mid && e <= r){
+            if(arr[s] <= arr[e]){
+                res[i] = arr[s];
                 i++;
-            } else {
-                arr[k] = R[j];
-                j++;
+                s++;
             }
-            k++;
+            else {
+                res[i] = arr[e];
+                i++;
+                e++;
+            }
         }
-
-        while (i < n1) {
-            arr[k] = L[i];
+        while(s <= mid) {
+            res[i] = arr[s];
             i++;
-            k++;
+            s++;
         }
-
-        while (j < n2) {
-            arr[k] = R[j];
-            j++;
-            k++;
+        
+        while(e <= r){
+            res[i] = arr[e];
+            i++;
+            e++;
+        }
+        
+        for(int j = l ; j <= r; j++ ){
+            arr[j] = res[j-l];
         }
     }
 }
