35d02e841a60e69cb0a9c0936512e27a5e1e4c9a
BALAJIMATHESWARAN <balajimatheswaran2004@gmail.com>
2025-07-14 22:35:56 +0530

    first commit

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+public class BinarySearch {
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,6,8,10};
+        int target = 4;
+        int l = 0;
+        int r = arr.length-1;
+        while(l <= r){
+            int mid = l+(r-l)/2;
+            if(arr[mid] == target){
+                System.out.println(mid);
+                break;
+            }
+            else if(arr[mid] > target) r = mid -1;
+            else l = mid +1;
+        }
+    }
+}
