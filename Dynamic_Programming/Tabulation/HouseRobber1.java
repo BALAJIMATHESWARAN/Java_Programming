1a6c09d9a3e23b2bfe3d7e67d12bff57d7ffc536
BALAJIMATHESWARAN <balajimatheswaran2004@gmail.com>
2025-09-04 11:12:56 +0530

    new Program

--- Dynamic_Programming/Tabulation/HouseRobber1.java
+++ Dynamic_Programming/Tabulation/HouseRobber1.java
@@ -1,5 +1,19 @@
 package Dynamic_Programming.Tabulation;
 
 public class HouseRobber1 {
-    
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,1};
+        int len = arr.length;
+        int dp [] = new int[len];
+        dp[0] = arr[0];
+        int pick = 0;
+        int notPick = 0;
+        for(int i = 1 ; i < len ; i++){
+            pick = arr[i];
+            if(i > 1) pick+=dp[i-2];
+            notPick = dp[i-1];
+            dp[i] = Math.max(pick,notPick);
+        }
+        System.out.println(dp[len-1]);
+    }
 }
