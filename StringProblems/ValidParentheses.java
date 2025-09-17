e126f3e0953f741938e5913476e2b61db7208d16
BALAJIMATHESWARAN <balajimatheswaran2004@gmail.com>
2025-09-17 15:44:02 +0530

    new code

---
+++ StringProblems/ValidParentheses.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class ValidParentheses {
+    public static void main(String[] args){
+        String str = "{]()}";
+        int len = str.length();
+        Stack<Character> s = new Stack<>();
+        for(int i = 0 ; i < len ; i++){
+            char ch = str.charAt(i);
+            if(ch == '{' || ch == '[' || ch == '('){
+                s.push(ch);
+            }
+            else if(ch == '}' || ch == ']' || ch == ')'){
+                char c = s.pop();
+                if((ch == '}' && c != '{')  || (ch == ']'  && c != '[') || ( ch == ')' && c != '(')){
+                    return ;
+                }
+            }
+        }
+        System.out.println(s.isEmpty());
+    }
+}
