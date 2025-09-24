fc045e7c5a348cf9c41175e66d763f7b9d8fda15
BALAJIMATHESWARAN <balajimatheswaran2004@gmail.com>
2025-09-24 21:17:40 +0530

    new program

--- SortingProblem/BubbleSort.java
+++ SortingProblem/BubbleSort.java
@@ -2,10 +2,10 @@ import java.util.*;
 class BubbleSort{
     public static void main(String [] args){
         int arr[]= {10,5,5,6,4,3,7,1,0};
-        for(int i = 0 ; i < arr.length;i++){
+        for(int i = 0 ; i < arr.length-1;i++){
             boolean swap = false;
             for(int j = 0 ;j < arr.length-i-1;j++){
-                if(arr[j] < arr[j+1]){
+                if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
